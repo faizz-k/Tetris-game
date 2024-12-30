14b248a8b7071b6a0e4dba91841831d0e36ee698
faizz-k <smartrahman0@gmail.com>
2024-12-30 15:26:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-let canvas = document.querySelector("#tetris");
-let scoreboard = document.querySelector("h2");
-let ctx = canvas.getContext("2d");
-ctx.scale(30,30);
+
 
 const SHAPES = [
     [
@@ -55,176 +52,174 @@ const COLORS = [
 const ROWS = 20;
 const COLS = 10;
 
+let canvas = document.querySelector("#tetris");
+let ctx = canvas.getContext("2d");
+ctx.scale(30,30);
+
+let pieceOBj = null;
 let grid = generateGrid();
-let fallingPieceObj = null;
-let score = 0;
 
-setInterval(newGameState,500);
-function newGameState(){
-    checkGrid();
-    if(!fallingPieceObj){
-        fallingPieceObj = randomPieceObject();
-        renderPiece();
-    }
-    moveDown();
-}
-
-function checkGrid(){
-    let count = 0;
-    for(let i=0;i<grid.length;i++){
-        let allFilled = true;
-        for(let j=0;j<grid[0].length;j++){
-            if(grid[i][j] == 0){
-                allFilled = false
-            }
-        }
-        if(allFilled){
-            count++;
-            grid.splice(i,1);
-            grid.unshift([0,0,0,0,0,0,0,0,0,0]);
-        }
-    }
-    if(count == 1){
-        score+=10;
-    }else if(count == 2){
-        score+=30;
-    }else if(count == 3){
-        score+=50;
-    }else if(count>3){
-        score+=100
-    }
-    scoreboard.innerHTML = "Score: " + score;
-}
-
-function generateGrid(){
-    let grid = [];
-    for(let i=0;i<ROWS;i++){
-        grid.push([]);
-        for(let j=0;j<COLS;j++){
-            grid[i].push(0)
-        }
-    }
-    return grid;
-}
-
-function randomPieceObject(){
+function generateRandomPiece() {
     let ran = Math.floor(Math.random()*7);
     let piece = SHAPES[ran];
     let colorIndex = ran+1;
     let x = 4;
     let y = 0;
-    return {piece,colorIndex,x,y}
+    return{piece,x,y,colorIndex};
+}
+
+setInterval(newGameState,500);
+
+function newGameState(){
+    checkGrid();
+    if(pieceOBj == null) {
+        
+        pieceOBj = generateRandomPiece();
+        renderPiece();
+    }
+    moveDown();
+}
+
+function checkGrid() {
+    for(let i =0;i<grid.length;i++) {
+        let allFilled = true;
+        for(let j =0;j<grid[i].length;j++) {
+            if(grid[i][j] == 0) {
+                allFilled = false
+            }
+        }
+        if(allFilled) {
+            grid.splice(i,1);
+            grid.unshift([0,0,0,0,0,0,0,0,0,0]);
+        }
+    }
 }
 
 function renderPiece(){
-    let piece = fallingPieceObj.piece;
-    for(let i=0;i<piece.length;i++){
-        for(let j=0;j<piece[i].length;j++){
-            if(piece[i][j] == 1){
-            ctx.fillStyle = COLORS[fallingPieceObj.colorIndex];
-            ctx.fillRect(fallingPieceObj.x+j,fallingPieceObj.y+i,1,1);
-        }
+    let piece = pieceOBj.piece;
+    for( let i =0;i<piece.length;i++) {
+        for(let j =0;j<piece[i].length;j++) {
+            if(piece[i][j] == 1) {
+                ctx.fillStyle = COLORS[pieceOBj.colorIndex];
+                ctx.fillRect(pieceOBj.x+j,pieceOBj.y+i,1,1);
+            }
         }
     }
 }
 
-function moveDown(){
-    if(!collision(fallingPieceObj.x,fallingPieceObj.y+1))
-        fallingPieceObj.y+=1;
+function moveDown() {
+    if(!collision(pieceOBj.x,pieceOBj.y+1))
+        pieceOBj.y+=1;
     else{
-        let piece = fallingPieceObj.piece
-        for(let i=0;i<piece.length;i++){
-            for(let j=0;j<piece[i].length;j++){
-                if(piece[i][j] == 1){
-                    let p = fallingPieceObj.x+j;
-                    let q = fallingPieceObj.y+i;
-                    grid[q][p] = fallingPieceObj.colorIndex;
+        for(let i =0;i<pieceOBj.piece.length;i++) {
+            for(let j=0;j<pieceOBj.piece[i].length;j++) {
+                if(pieceOBj.piece[i][j] == 1) {
+                    let p = pieceOBj.x+j;
+                    let q = pieceOBj.y+i;
+
+                    grid[q][p] = pieceOBj.colorIndex;
                 }
             }
         }
-        if(fallingPieceObj.y == 0){
-            alert("gamer over");
+        if(pieceOBj.y == 0) {
+            alert("Game Over!!!");
             grid = generateGrid();
-            score = 0;
         }
-        fallingPieceObj = null;
+        pieceOBj = null;
     }
-    renderGame();
+    renderGrid();
 }
 
-function moveLeft(){
-    if(!collision(fallingPieceObj.x-1,fallingPieceObj.y))
-        fallingPieceObj.x-=1;
-    renderGame();
+function moveLeft() {
+    if(!collision(pieceOBj.x-1,pieceOBj.y))
+        pieceOBj.x-=1;
+    
+    renderGrid();
 }
 
-function moveRight(){
-    if(!collision(fallingPieceObj.x+1,fallingPieceObj.y))
-        fallingPieceObj.x+=1;
-    renderGame();
+function moveRight() {
+    if(!collision(pieceOBj.x+1,pieceOBj.y))
+        pieceOBj.x+=1;
+    renderGrid();
 }
 
-function rotate(){
+function rotate() {
     let rotatedPiece = [];
-    let piece = fallingPieceObj.piece;
-    for(let i=0;i<piece.length;i++){
+    let piece = pieceOBj.piece;
+
+    for(let i = 0;i<piece.length;i++) {
         rotatedPiece.push([]);
-        for(let j=0;j<piece[i].length;j++){
+        for(let j =0;j<piece[i].length;j++) {
             rotatedPiece[i].push(0);
         }
     }
-    for(let i=0;i<piece.length;i++){
-        for(let j=0;j<piece[i].length;j++){
-            rotatedPiece[i][j] = piece[j][i]
+
+    for(let i =0;i<piece.length;i++) {
+        for(j =0;j<piece[i].length;j++) {
+            rotatedPiece[i][j] = piece[j][i];
         }
     }
 
-    for(let i=0;i<rotatedPiece.length;i++){
+    for(let i=0;i<rotatedPiece.length;i++) {
         rotatedPiece[i] = rotatedPiece[i].reverse();
     }
-    if(!collision(fallingPieceObj.x,fallingPieceObj.y,rotatedPiece))
-        fallingPieceObj.piece = rotatedPiece
-    renderGame()
+
+    if(!collision(pieceOBj.x,pieceOBj.y,rotatedPiece)) {
+        pieceOBj.piece = rotatedPiece;
+    }
+    renderGrid();
 }
 
-function collision(x,y,rotatedPiece){
-    let piece = rotatedPiece || fallingPieceObj.piece
-    for(let i=0;i<piece.length;i++){
-        for(let j=0;j<piece[i].length;j++){
-            if(piece[i][j] == 1){
-            let p = x+j;
-            let q = y+i;
-            if(p>=0 && p<COLS && q>=0 && q<ROWS){
-                if(grid[q][p]>0){
+function collision(x,y,rotatedPiece) {
+    let piece = rotatedPiece || pieceOBj.piece;
+    for(let i=0;i<piece.length;i++) {
+        for(let j=0;j<piece[i].length;j++) {
+            if(piece[i][j] == 1) {
+                let p = x+j;
+                let q = y+i;
+                if(p>=0 && p<COLS && q>=0 && q<ROWS) {
+                    if(grid[q][p]>0) {
+                        return true;
+                    }
+                }else {
                     return true;
-                }
-            }else{
-                return true;
-            }}
+                } 
+            }
         }
     }
     return false;
 }
 
-function renderGame(){
-    for(let i=0;i<grid.length;i++){
-        for(let j=0;j<grid[i].length;j++){
+function generateGrid() {
+    let grid = [];
+    for(let i = 0;i<ROWS;i++) {
+        grid.push([]);
+        for(let j =0;j<COLS;j++) {
+            grid[i].push(0);
+        }
+    }
+    return grid;
+}
+
+function renderGrid() {
+    for(let i =0;i<grid.length;i++) {
+        for(let j= 0;j<grid[i].length;j++) {
             ctx.fillStyle = COLORS[grid[i][j]];
-            ctx.fillRect(j,i,1,1)
+            ctx.fillRect(j,i,1,1);
         }
     }
     renderPiece();
 }
 
-document.addEventListener("keydown",function(e){
-    let key = e.key;
-    if(key == "ArrowDown"){
+document.addEventListener("keydown", function(e) {
+    let key = e.code;
+    if(key== "ArrowDown") {
         moveDown();
-    }else if(key == "ArrowLeft"){
+    }else if(key=="ArrowLeft") {
         moveLeft();
-    }else if(key == "ArrowRight"){
+    } else if(key=="ArrowRight") {
         moveRight();
-    }else if(key == "ArrowUp"){
+    } else if(key=="ArrowUp") {
         rotate();
     }
 })
